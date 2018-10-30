8914aee96df527ce9335dba0af938b3efc18b61b
Lee DeBoom <lee@kicksite.net>
2018-10-29 22:48:43 -0500

    the logic

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'haml-rails'
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,12 @@
 function doStuff() {
-  $('#after').removeClass('hidden');
-  $('#before').addClass('hidden');
-  $('body').addClass('fail');
+  $("#before").effect("shake", {times: 10}, 1000, function() {
+    var num = Math.floor((Math.random() * 100) + 1)
+    if(num < 75 && (num % 2) == 0) {
+      $('#after').removeClass('hidden');
+      $('#before').addClass('hidden');
+      $('body').addClass('fail');
+    }
+  });
 }
 
 function resetStuff() {
