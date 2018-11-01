f13f916183f3c0f57c60162e7b7f10b8702a50a5
Lee DeBoom <lee@kicksite.net>
2018-10-31 21:19:48 -0500

    remove the shake

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,23 @@
 function doStuff() {
-  $("#before").effect("shake", {times: 10}, 1000, function() {
-    var prob = 0.2; // prob = percent probability / 100; e.g. 30% -> 0.3
-    if(Math.random() < prob) {
-      $('#after').removeClass('hidden');
-      $('#before').addClass('hidden');
-      $('body').addClass('fail');
-    }
-  });
+  var prob = 0.2; // prob = percent probability / 100; e.g. 30% -> 0.3
+  if(Math.random() < prob) {
+    $('#after').removeClass('hidden');
+    $('#before').addClass('hidden');
+    $('body').addClass('fail');
+  } else {
+    $('#before').animate({
+      left: '150%'
+    }, 500, function() {
+        $('#before').css('left', '-50%');
+        $('#before').animate({
+          left: '50%'
+        }, 500, function() {});
+    });
+
+    $('#before').next().animate({
+        left: '50%'
+    }, 500);
+  }
 }
 
 function resetStuff() {
